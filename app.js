457cdc416373bab63cd44802bb01d40ad441d821
MabelhH <clarahuanaco20@gmail.com>
2025-08-06 23:36:57 -0500

    juego de sorteo de amigos terminado

--- app.js
+++ app.js
@@ -12,29 +12,56 @@ function agregarAmigo() {
         input.classList.remove('error');
     }
 
+    if (amigos.includes(amigo)) {
+         alert('Ese amigo ya fue agregado.');
+         limpiarImput();
+        return;
+    }
+
     let lista = document.getElementById('listaAmigos');
     let li = document.createElement('li');
     li.textContent = amigo;
     lista.appendChild(li);
     amigos.push(amigo);
-    //console.log(amigos); 
+    console.log(amigos); 
     limpiarImput();
 }
 function limpiarImput(){
   document.querySelector('#amigo').value ='';
 }
-
+function limpiarresultado(){
+    let resultado = document.getElementById('resultadoSorteo');
+    resultado.textContent = '';
+    resultado.style.display = 'none'; 
+}
 
 function sortearAmigo() {
-   
+   if (amigos.length === 0) {
+        alert('La lista de amigos está vacía.');
+        limpiarresultado();
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById('resultadoSorteo');
+    resultado.textContent = `El amigo secreto sorteado es: ${amigoSorteado}`;
+    console.log(`El amigo secreto sorteado es: ${amigoSorteado}`);
+
+
+
 }
+
+
     
 // Listener para eliminar un amigo si haces clic sobre su nombre
 document.getElementById('listaAmigos').addEventListener('click', function(event) {
     if (event.target.tagName === 'LI') {
         event.target.remove();
         amigos.splice(amigos.indexOf(event.target.textContent), 1);
-        //console.log(amigos);
+        console.log(amigos);
+    
+        console.log(amigos);
     }
 });
-
