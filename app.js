ab826e4725f5f29dfb67d38bdf2c3a4771017671
MabelhH <clarahuanaco20@gmail.com>
2025-08-06 21:21:31 -0500

    tercer  commit del juego

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
- function agregarAmigo(){
-
-   const  input = document.getElementById('amigo');
-   const  amigo = input.value.trim();
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const amigo = input.value.trim();
+    
     if (amigo === '') {
         alert('Por favor, ingresa un nombre de amigo.');
         return;
-    }else{
-        console.log("Nombre añadido:", nombre);
-        input.value = "";
     }
- }// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+    const lista = document.getElementById('listaAmigos'); // ID corregido
+    const li = document.createElement('li');
+    li.textContent = amigo;
+    lista.appendChild(li);
+    input.value = ''; // Línea corregida
+}
+
 function sortearAmigo(){
   // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 }
