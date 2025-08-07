5f695aacfde178e2f5acfa95f2bdfd2f8f13a289
MabelhH <clarahuanaco20@gmail.com>
2025-08-06 21:42:41 -0500

    cuarto  commit del juego

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const amigo = input.value.trim();
-    
+
     if (amigo === '') {
+        input.classList.add('error');
         alert('Por favor, ingresa un nombre de amigo.');
         return;
+    }else {
+        input.classList.remove('error');
     }
 
-    const lista = document.getElementById('listaAmigos'); // ID corregido
+    const lista = document.getElementById('listaAmigos');
     const li = document.createElement('li');
     li.textContent = amigo;
     lista.appendChild(li);
-    input.value = ''; // Línea corregida
+    input.value = '';
 }
 
-function sortearAmigo(){
-  // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function sortearAmigo() {
+
 }
+    
+// Listener para eliminar un amigo si haces clic sobre su nombre
+document.getElementById('listaAmigos').addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.remove();
+    }
+});
