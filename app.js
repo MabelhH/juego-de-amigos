4e8281ec5f044d23ebf6f9621266b359889bac18
MabelhH <clarahuanaco20@gmail.com>
2025-08-06 20:57:02 -0500

    primer commit del juego

---
+++ app.js
@@ -0,0 +1,12 @@
+ function agregarAmigo(){
+
+   let  input = document.getElementById('amigo');
+   var amigo = input.value.trim;
+    if (amigo === '') {
+        alert('Por favor, ingresa un nombre de amigo.');
+        return;
+        }
+ }// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function sortearAmigo(){
+  // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+}
